c35fc484dd8d15aeef21805e6b70fe43ed6e841e
jerome <jerome_jero@t-online.de>
2023-11-24 23:36:44 +0100

    unfinished validation loss plot

--- train.py
+++ train.py
@@ -35,34 +35,38 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    plt.ion()
-    fig, ax = plt.subplots()
     losses = []
+    validation_losses = []
 
     # Training the model
     for epoch in range(10):
-        for y, labels in dataloader:
-            y, labels = y.to(device), labels.to(device)
+        for y, label in dataloader:
+            # Training
+            y, label = y.to(device), label.to(device)
             optimizer.zero_grad()
             y_hat = net(y)
-            loss = criterion(y_hat, labels)
+            loss = criterion(y_hat, label)
             loss.backward()
             optimizer.step()
-
             losses.append(loss.item())
 
-            # Update the plot every few iterations
-            if len(losses) % 10 == 0:
-                ax.clear()
-                ax.plot(losses, label='Training Loss')
-                ax.set_xlabel('Iterations')
-                ax.set_ylabel('Loss')
-                ax.legend()
-                plt.pause(0.1)  # Add a short pause to update the plot
+            # Validation
+            # TODO: Faster load of validation, maybe no_grad
+            random_image, random_label = next(iter(dataloader))  # Takes way to long
+            random_image, random_label = random_image.to(device), random_label.to(device)
+            validation = net(random_image)
+            validation_loss = criterion(validation, random_label)
+            validation_losses.append(validation_loss.item())
 
         print(f'{epoch}: Loss: {losses[-1]}')
 
     print('Finished Training')
-    torch.save(net.state_dict(), 'squeeze_rotated.pth')
-    plt.ioff()
+
+    plt.plot(range(len(losses)), losses, label='Training Loss')
+    plt.plot(range(len(validation_losses)), validation_losses, label='Validation Loss')
+    plt.xlabel('Iterations')
+    plt.ylabel('Loss')
+    plt.legend()
     plt.show()
+
+    torch.save(net.state_dict(), 'squeeze_rotated.pth')
