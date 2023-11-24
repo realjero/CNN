3d30c20a1928281f816d9ed505654cddc1cac4c1
jerome <jerome_jero@t-online.de>
2023-11-25 00:37:01 +0100

    cuda

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ class SqueezeNet:
         self.net.classifier[1] = nn.Conv2d(512, 4, kernel_size=(1, 1))
 
         if weights:
-            self.net.load_state_dict(torch.load(weights, map_location=torch.device('cpu')))
+            self.net.load_state_dict(torch.load(weights))
 
     def get_model(self):
         return self.net
